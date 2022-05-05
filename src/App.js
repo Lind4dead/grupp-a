fde50c56b0b8e7f9a2169e55c00a29d433e2cff7
unknown <emil.lind@utb.ecutbildning.se>
2022-05-05 13:26:14 +0200

    omfördelat i filerna

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { getProducts } from './store/actions/productsAction';
 import './App.css';
-import ProductCard from './components/ProductCard';
+
 
 function App() {
 
@@ -13,17 +13,10 @@ function App() {
 
   }, [dispatch])
 
-  const { data: products, loading } = useSelector(state => state.products)
-
+  
   return (
     <div className="App">
-      <div className='container'>
-        <div className="row">
-          {
-            products.map(product => <div className='col-6 col-md-4 col-xl-3'><ProductCard key={product.id} product={product} /></div>)
-          }
-        </div>
-      </div>
+      
 
     </div>
   );
--- src/views/ProductsGridView.jsx
+++ src/views/ProductsGridView.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import ProductCard from '../components/ProductCard'
 
-const ProductsListView = () => {
+
+const ProductsGridView = () => {
+
+
+  const { data: products, loading } = useSelector(state => state.products)
+
+
   return (
     <div className='container'>
         <div className="row">
+          { loading && <p>Loading...</p>}
           {
             products.map(product => <div className='col-6 col-md-4 col-xl-3'><ProductCard key={product.id} product={product} /></div>)
           }
@@ -13,4 +21,4 @@ const ProductsListView = () => {
   )
 }
 
-export default ProductsListView
+export default ProductsGridView
