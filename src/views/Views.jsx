926ea8ca85c02fb22258717348196a3b64aa77b4
Lindblaa <caroline.lindbla@utb.ecutbildning.se>
2022-05-10 13:47:11 +0200

    Alla spritsidor ready to go, länkar i menyn till spritsorterna och även tillbaka till alla produkter från vardera view

--- src/views/Views.jsx
+++ src/views/Views.jsx
@@ -3,6 +3,12 @@ import { Route, Routes } from 'react-router-dom'
 import HomeView from './HomeView'
 import LoginView from './LoginView'
 import ProductsGridView from './ProductsGridView'
+import TequilaView from './Spritsorter/TequilaView'
+import VodkaView from './Spritsorter/VodkaView'
+import WhiskyView from './Spritsorter/WhiskyView'
+import LiquorView from './Spritsorter/LiquorView'
+import RomView from './Spritsorter/RomView'
+
 
 const Views = () => {
     return (
@@ -10,6 +16,11 @@ const Views = () => {
         <Route path='/' element={ <HomeView /> } />
         <Route path='/login' element={<LoginView />} />
         <Route path='/allproducts' element={<ProductsGridView />} />
+        <Route path='/tequila' element={<TequilaView />} />
+        <Route path='/vodka' element={<VodkaView />} />
+        <Route path='/whisky' element={<WhiskyView />} />
+        <Route path='/liquor' element={<LiquorView />} />
+        <Route path='/rom' element={<RomView />} />
       </Routes>
     )
 } 
