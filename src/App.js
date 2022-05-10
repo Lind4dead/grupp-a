6b38a2bdd48d840d0b53e61e9a554368d24a7a7c
Jonas <jonasengstrom_@hotmail.com>
2022-05-10 11:00:34 +0200

    details

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import ProductDetails from './views/ProductDetails';
 
 function App() {
+
+
   return (
     <div className="App">
       
+      <ProductDetails /> 
+  
     </div>
   );
 }
