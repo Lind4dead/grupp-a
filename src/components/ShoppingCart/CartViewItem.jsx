15fbcc750b20cbba2f18b9c2a439db6d511ff435
Rohny <ecu_runy@hotmail.com>
2022-05-18 00:01:04 +0200

    CartView

---
+++ src/components/ShoppingCart/CartViewItem.jsx
@@ -0,0 +1,35 @@
+import CartSelect from "./CartSelect";
+import { deleteItem } from "../../store/actions/shoppingCartActions";
+import { useDispatch } from "react-redux";
+import "./CartViewItem.css";
+
+const CartItem = ({ product }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="one">
+      <button onClick={() => dispatch(deleteItem(product._id))} className="btn btn-danger btn-sm mb-3 py-2">
+        <i className="fa-solid fa-trash-can fs-6"></i>
+      </button>
+      <div className="image">
+        <img src={product.imgUrl} alt="Limoncello di Capri" />
+      </div>
+      <div className="wrapper-info">
+        <div className="text">
+          <p className="fw-bold">
+            {product.title + " "}
+            <small className="fw-lighter col-11 ">
+              {product.container} - {product.alcPercentage}
+            </small>
+          </p>
+        </div>
+
+        <div>
+          <CartSelect product={product} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
--- src/views/Views.jsx
+++ src/views/Views.jsx
@@ -1,36 +1,38 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import HomeView from './HomeView'
-import LoginView from './LoginView'
-import ProductDetails from './ProductDetails'
-import ProductsGridView from './ProductsGridView'
-import TequilaView from './Spritsorter/TequilaView'
-import VodkaView from './Spritsorter/VodkaView'
-import WhiskyView from './Spritsorter/WhiskyView'
-import LiquorView from './Spritsorter/LiquorView'
-import RomView from './Spritsorter/RomView'
-import MinaSidor from '../components/MinaSidor'
-import AdminUserOrdersView from './AdminUserOrdersView'
-import AdminView from './AdminView'
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomeView from "./HomeView";
+import LoginView from "./LoginView";
+import ProductDetails from "./ProductDetails";
+import ProductsGridView from "./ProductsGridView";
+import TequilaView from "./Spritsorter/TequilaView";
+import VodkaView from "./Spritsorter/VodkaView";
+import WhiskyView from "./Spritsorter/WhiskyView";
+import LiquorView from "./Spritsorter/LiquorView";
+import RomView from "./Spritsorter/RomView";
+import MinaSidor from "../components/MinaSidor";
+import AdminUserOrdersView from "./AdminUserOrdersView";
+import AdminView from "./AdminView";
+import CartView from "../components/ShoppingCart/CartView";
 
 const Views = () => {
-    return (
-      <Routes>
-        <Route path='/' element={ <HomeView /> } />
-        <Route path='/login' element={<LoginView />} />
-        <Route path='/allproducts' element={<ProductsGridView />} />
-        <Route path='/tequila' element={<TequilaView />} />
-        <Route path='/vodka' element={<VodkaView />} />
-        <Route path='/whisky' element={<WhiskyView />} />
-        <Route path='/liquor' element={<LiquorView />} />
-        <Route path='/rom' element={<RomView />} />
-        <Route path='/details/:id' element={<ProductDetails />} />
-        <Route path='/minasidor' element={ <MinaSidor /> } />
-        <Route path='/admin' element={ <AdminView /> } />
-        <Route path='/administrera' element={ <AdminUserOrdersView /> } />
-      </Routes>
-    )
-} 
+  return (
+    <Routes>
+      <Route path="/" element={<HomeView />} />
+      <Route path="/login" element={<LoginView />} />
+      <Route path="/allproducts" element={<ProductsGridView />} />
+      <Route path="/tequila" element={<TequilaView />} />
+      <Route path="/vodka" element={<VodkaView />} />
+      <Route path="/whisky" element={<WhiskyView />} />
+      <Route path="/liquor" element={<LiquorView />} />
+      <Route path="/rom" element={<RomView />} />
+      <Route path="/details/:id" element={<ProductDetails />} />
+      <Route path="/minasidor" element={<MinaSidor />} />
+      <Route path="/admin" element={<AdminView />} />
+      <Route path="/administrera" element={<AdminUserOrdersView />} />
 
-export default Views
+      <Route path="/cart" element={<CartView />} />
+    </Routes>
+  );
+};
+
+export default Views;
