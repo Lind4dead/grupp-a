015c3667c8a12ff41f71d82cd4e1362f6117a41b
unknown <emil.lind@utb.ecutbildning.se>
2022-05-17 11:00:31 +0200

    breadcrumbs fixade

--- src/views/ProductDetails.jsx
+++ src/views/ProductDetails.jsx
@@ -1,6 +1,6 @@
 // import {useState} from 'react'
 // import ImgModal from '../components/Modal/ImgModal'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { getProductById } from '../store/actions/singleProductAction'
@@ -15,11 +15,14 @@ const ProductDetails = () => {
 
   const { data: product, loading, error } = useSelector(state => state.singleProduct)
 
+  const capitalizeFirstLetter = ([first, ...rest], locale = navigator.language) =>
+  first.toLocaleUpperCase(locale) + rest.join('')
 
+  
   useEffect(() => {
     dispatch(getProductById(id))
   }, [dispatch, id])
-
+  
 
   return ( 
   
@@ -34,7 +37,7 @@ const ProductDetails = () => {
             <div className='d-none d-md-block mb-2'>
               <Link to='/allproducts'><small className='h5 fw-light me-md-3 text-dark'>Produkter</small></Link>
               <i className="fa-solid fa-angle-right me-md-3"></i> 
-              <Link to={`/${product.type}`}><small className='h5 fw-light me-md-3 text-dark'>{product.type}</small></Link>
+              <Link to={`/${product.type}`}><small className='h5 fw-light me-md-3 text-dark'>{ product.type === "liquor" ? "Likör" : capitalizeFirstLetter(product.type)}</small></Link>
               <i className="fa-solid fa-angle-right me-md-3"></i>
               <Link to={''}><small className='text-underline disable-link h5 fw-normal me-md-3 text-dark'>{product.title}</small></Link>
               
