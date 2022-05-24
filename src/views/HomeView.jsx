ccea70f167df391ba6ccd61909081d8c556ff5c3
unknown <emil.lind@utb.ecutbildning.se>
2022-05-24 10:06:28 +0200

    bilderna på homeview funkar, hoppas jag

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -13,20 +13,20 @@ const HomeView = () => {
 
         {/* <!-- Gallery --> */}
     <div className='container d-none d-sm-block'>
-      <div className="bildrad1">
-        <div className="col-6 me-2 card ">
+      <div className="bildrad1 row gap-2">
+        <div className="col-6 card">
           <img
             src="https://cdn.systembolaget.se/499326/globalassets/dryck-och-mat/kombinera-dryck-och-mat/50-vin-till-grillad-mat.jpg?w=2000"
-            className="w-100 shadow-1-strong"
+            className="w-100 shadow-1-strong object-fit-contain"
             alt="Grillad sparris"
           />
           <div className="bild-box mask"><span className="box-titel">EN HET FRÅGA <p className="box-text">Vilken drink till grillat?</p></span></div>
         </div>
 
-        <div className="col me-2 card">
+        <div className="col card">
           <img
             src="https://cdn.systembolaget.se/49632e/globalassets/dryck-och-mat/smakskolan/25-kaldolmar.jpg?w=320"
-            className="w-100 shadow-1-strong"
+            className="w-100 shadow-1-strong object-fit-contain"
             alt="Kåldolmar"
           />
            <div className="bild-box mask"><span className="box-titel">SMAKSKOLAN <p className="box-text">Kombinera sprit och mat</p></span></div>
@@ -42,8 +42,8 @@ const HomeView = () => {
         </div>
       </div>
 
-      <div className="bildrad2">
-        <div className="col me-1 card">
+      <div className="bildrad2 gap-2 row">
+        <div className="col card">
           <img
             src="https://cdn.systembolaget.se/4a8786/globalassets/dryck-och-mat/varldens-mat/50-bahnmi.jpg?w=768"
             className="w-100 shadow-1-strong"
@@ -52,7 +52,7 @@ const HomeView = () => {
           <div className="bild-box mask"><span className="box-titel">LÄTT OCH HETT <p className="box-text">Dryck till vietnamesisk mat</p></span></div>
         </div>
 
-         <div className="col ms-2 card">
+         <div className="col card">
           <img
             src="https://cdn.systembolaget.se/4a7d02/globalassets/dryck-och-mat/kombinera-dryck-och-mat/50-ost.jpg?w=320"
             className="w-100 shadow-1-strong"
